449b44819219704e170a8c53477402efad63b1e9
MelidaZuniga55 <melidazuniga55@outlook.com>
2025-07-28 11:43:13 -0600

    estructura del proyecto y configuracion del layout

---
+++ src/main/java/org/esfe/GestionDeTareas/GestionDeTareasApplication.java
@@ -0,0 +1,14 @@
+package org.esfe.GestionDeTareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeTareasApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GestionDeTareasApplication.class, args);
+	}
+
+}
