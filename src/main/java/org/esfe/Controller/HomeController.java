cde8cf8278a73a2b1690f73c916050e74a4e4512
MelidaZuniga55 <melidazuniga55@outlook.com>
2025-07-31 08:03:23 -0600

    funcionalidad completa para el modelo Grupo

--- src/main/java/org/esfe/Controller/HomeController.java
+++ src/main/java/org/esfe/Controller/HomeController.java
@@ -1,13 +1,15 @@
 package org.esfe.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    public String index()
-    {
-        return "index";
+
+    @GetMapping
+    public String index() {
+        return "home/index";
     }
 }
